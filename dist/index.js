471cc33fa5d1c936e990c2964ebf5f045eccf645
Vitalii Drag <ruban.vitaliy@gmail.com>
2020-07-31 18:29:23 +0300

    Ignoring dist

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var test_special_file_1 = require("test.special-file");
-console.log(test_special_file_1.hello() + " " + test_special_file_1.test());
